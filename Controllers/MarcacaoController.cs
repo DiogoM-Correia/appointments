395e322e69a4d47bcfa066aea0675afa98e8d9bf
DiogoM-Correia <55164816+DiogoM-Correia@users.noreply.github.com>
2023-12-25 21:30:24 +0000

    review paths views

---
+++ Controllers/MarcacaoController.cs
@@ -0,0 +1,37 @@
+﻿using Appointments.Data;
+using Appointments.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Appointments.Controllers
+{
+    public class MarcacaoController : Controller
+    {
+        private readonly ILogger<MarcacaoController> _logger;
+
+        public MarcacaoController(ILogger<MarcacaoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Authorize]
+        public IActionResult Book()
+        {
+            return View();
+        }
+
+        public class ApplicationUser : IdentityUser
+        {
+            public ICollection<Appointment> Appointments { get; set; }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+}
